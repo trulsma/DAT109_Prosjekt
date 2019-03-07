3096bc9a0414a6c6d4d36c492735f62656bdcac5
Espen Haugland Mæland <espenhauglandmaeland@158-37-226-139.hvl.no>
2019-03-07 15:35:42 +0100

    Updated database script

--- SQL/Database.sql
+++ SQL/Database.sql
@@ -4,25 +4,27 @@ SET search_path TO prosjekt1;
 
 CREATE TABLE Bedrift (
   bedriftid          SERIAL,
-  bedriftnavn        VARCHAR(100),
+  bedriftnavn        VARCHAR(100) UNIQUE,
   bedriftbeskrivelse VARCHAR(1000),
-  CONSTRAINT uniqueBedriftNavn UNIQUE (bedriftnavn)
+  CONSTRAINT bedriftPK PRIMARY KEY (bedriftid)
 );
 
 CREATE TABLE Prosjekt (
   prosjektid          SERIAL,
-  prosjektnavn        VARCHAR(100),
+  prosjektnavn        VARCHAR(100) UNIQUE,
   prosjektbeskrivelse VARCHAR(1000),
   sammarbeidsbedrift  INTEGER,
   qrcodeurl           VARCHAR(255),
-  CONSTRAINT uniqueProsjektNavn UNIQUE (prosjektnavn),
-  CONSTRAINT bedriftFK FOREIGN KEY (sammarbeidsbedrift) REFERENCES Bedrift (bedriftid)
+  CONSTRAINT bedriftFK FOREIGN KEY (sammarbeidsbedrift) REFERENCES Bedrift (bedriftid),
+  CONSTRAINT prosjektPK PRIMARY KEY (prosjektid)
+
 );
 
 CREATE TABLE Stemme (
-  stemmeid SERIAL,
-  prosjektid INTEGER,
-  epost VARCHAR(255),
+  stemmeid    SERIAL,
+  prosjektid  INTEGER,
+  epost       VARCHAR(255),
   stemmeverdi INTEGER,
-  CONSTRAINT prosjektidFK FOREIGN KEY (prosjektid) REFERENCES Prosjekt(prosjektid)
+  CONSTRAINT prosjektidFK FOREIGN KEY (prosjektid) REFERENCES Prosjekt (prosjektid),
+  CONSTRAINT stemmePK PRIMARY KEY (stemmeid)
 );
