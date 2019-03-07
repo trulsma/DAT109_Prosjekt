edc26987146d3b1a204c1f1cf1fa752852287e73
S571736 <Sondre.Gjesdal@gmail.com>
2019-03-07 12:38:32 +0100

    test

--- src/Setup.java
+++ src/Setup.java
@@ -2,5 +2,7 @@ public class Setup {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        SuperWriter writer = new SuperWriter();
+        writer.messenger("Testing");
     }
 }
---
+++ src/SuperWriter.java
@@ -0,0 +1,8 @@
+public class SuperWriter {
+    public SuperWriter(){
+
+    }
+    public void messenger(String message){
+        System.out.println(message);
+    }
+}
