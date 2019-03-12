ffc7a386cbebfdf136c61526bff175c46bda2dbc
Enah <182255@stud.hvl.no>
2019-03-12 11:00:31 +0100

    begynner med implementering av StemmeController

--- src/main/java/no/hvl/dat109/spring/service/Interfaces/IStemmeService.java
+++ src/main/java/no/hvl/dat109/spring/service/Interfaces/IStemmeService.java
@@ -1,5 +1,18 @@
 package no.hvl.dat109.spring.service.Interfaces;
 
+import no.hvl.dat109.spring.beans.StemmeBean;
+
+import java.util.List;
+
 public interface IStemmeService {
     //TODO lag stemme interface
+
+     void addStemme(String epost, Integer stemmeverdi);
+
+    List<StemmeBean> getStemmerIEnBedrift(int bedriftId);
+
+
+
+
+
 }
--- src/main/java/no/hvl/dat109/spring/service/StemmeService.java
+++ src/main/java/no/hvl/dat109/spring/service/StemmeService.java
@@ -1,9 +1,12 @@
 package no.hvl.dat109.spring.service;
 
+import no.hvl.dat109.spring.beans.StemmeBean;
 import no.hvl.dat109.spring.repository.StemmeRepository;
 import no.hvl.dat109.spring.service.Interfaces.IStemmeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StemmeService implements IStemmeService {
@@ -11,4 +14,15 @@ public class StemmeService implements IStemmeService {
 
     @Autowired
     private StemmeRepository stemmeRepository;
+
+
+    @Override
+    public void addStemme(String epost, Integer stemmeverdi) {
+        stemmeRepository.save(new StemmeBean(epost, stemmeverdi));
+    }
+
+    @Override
+    public List<StemmeBean> getStemmerIEnBedrift(int bedriftId) {
+        return null;
+    }
 }
