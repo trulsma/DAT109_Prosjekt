670d18cef28847baa2f85d8b12ee104da9994de1
h183502 <heine.fjeld@gmail.com>
2019-03-12 13:20:18 +0100

    Oppdattert html for registrer_bedrift og nye metoder i bedriftService/Ibedriftservis/bedriftcontroller

--- src/main/java/no/hvl/dat109/spring/controller/BedriftController.java
+++ src/main/java/no/hvl/dat109/spring/controller/BedriftController.java
@@ -2,12 +2,15 @@ package no.hvl.dat109.spring.controller;
 
 import no.hvl.dat109.prosjekt.Processing;
 import no.hvl.dat109.spring.beans.BedriftBean;
+import no.hvl.dat109.spring.service.BedriftService;
 import no.hvl.dat109.spring.service.Interfaces.IBedriftService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BedriftController {
@@ -31,17 +34,33 @@ public class BedriftController {
         return "index";
     }
 
-    @GetMapping("/bedrift/add/{navn}/{beskrivelse}")
+    /*@GetMapping("/bedrift/add/{navn}/{beskrivelse}")
     public String addBedrift(@PathVariable("navn") String navn, @PathVariable("beskrivelse") String beskrivelse, Model model) {
 
         //If for some reason database is empty again, you can easily add new one for debugging
 
         if (repository.exists(navn)) return "redirect:/bedrifter";
-        repository.addBedrift(navn, beskrivelse);
+        repository.addBedrift(new navn, beskrivelse);
         model.addAttribute("test", repository.getAll());
 
         //example of redirect
         return "redirect:/bedrifter";
+    }*/
+
+    @GetMapping("/bedrift/add")
+    String addBedrift(){
+        return "registrer_bedrift";
+    }
+
+    @PostMapping("/bedrift/add")
+    String addBedriftPostRequest(
+                                 @RequestParam String bedriftnavn,
+                                 @RequestParam String bedriftbeskrivelse){
+
+        BedriftBean bedrift = new BedriftBean(bedriftnavn, bedriftbeskrivelse);
+        repository.addBedrift(bedrift);
+
+        return "redirect:/bedrift/" + bedrift.getBedriftid();
     }
 
 
