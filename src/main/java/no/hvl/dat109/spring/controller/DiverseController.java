4bf112be9301371a4b7c4c494cd9306dd7e266a7
S571736 <Sondre.Gjesdal@gmail.com>
2019-03-14 12:59:44 +0100

    removed things in DiverseController

--- src/main/java/no/hvl/dat109/spring/controller/DiverseController.java
+++ src/main/java/no/hvl/dat109/spring/controller/DiverseController.java
@@ -9,27 +9,8 @@ import javax.servlet.http.HttpSession;
 public class DiverseController {
 
 
-    @PostMapping("/registrer_deg")
-    String registrerBruker(@RequestParam String epost, HttpSession session) {
-        session.setAttribute("epost", epost);
-
-        return "index";
-    }
 
 
-    @GetMapping("/registrer_deg")
-    public String getRegistrerBruker() {
-        return "registrer_deg";
-    }
-
-
-    @GetMapping("/index")
-    String getNoe(HttpSession session) {
-        if (session.getAttribute("epost") == null) {
-            return "redirect:/registrer_deg";
-        }
-        return "index";
-    }
 }
 
 
