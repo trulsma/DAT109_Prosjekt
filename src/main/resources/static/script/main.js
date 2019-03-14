b9a96941a155257da0d4704cfa62cdd0c59b4fa3
S571736 <Sondre.Gjesdal@gmail.com>
2019-03-14 14:59:55 +0100

    finally statistics working

--- src/main/resources/static/script/main.js
+++ src/main/resources/static/script/main.js
@@ -1,6 +1,22 @@
-var ctx = 'myLines'
+var ctx = document.getElementById('myLines');
 var myLines = new Chart(ctx, {
     type: 'line',
-    data: [20, 10, 30],
-    options: options
+    data: {
+        labels: ['verdi_1', 'verdi_2', 'verdi_3'],
+        datasets: [{
+            label: 'fin test',
+            data: [20, 10, 30],
+            backgroundColor: [
+                'rgba(255, 0, 255, 0.3)',
+                'rgba(128,0,0, 0.3)',
+                'rgba(0,0,128, 0.3)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
 });
