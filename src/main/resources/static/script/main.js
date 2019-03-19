6e20de8d4e1a2bb52ace2c0d5590a8b809b12612
S571736 <Sondre.Gjesdal@gmail.com>
2019-03-19 12:06:11 +0100

    sweet home alabama

--- src/main/resources/static/script/main.js
+++ src/main/resources/static/script/main.js
@@ -6,72 +6,57 @@ function randomRGBA() {
 
     return 'rgba(' + o(r() * m) + ',' + o(r() * m) + ',' + o(r() * m) + ',';
 }
-$
-console.log("#reee");
-var random1 = randomRGBA();
-var random2 = randomRGBA();
-var random3 = randomRGBA();
 
-var myBar = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['prosjekt 1', 'prosjekt 2', 'prosjekt 3'],
-        datasets: [{
-            label: ['prosjekt 1', 'prosjekt 2', 'prosjekt 3'],
-            data: ["#{prosjekt.prosjektid}", 1, 2.5],
-            borderColor: [
-                random1 + '1)',
-                random2 + '1)',
-                random3 + '1)'
-            ],
-            backgroundColor: [
-                random1 + '0.1)',
-                random2 + '0.1)',
-                random3 + '0.1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {scales: {yAxes: [{ticks: {
-        beginAtZero: true,
-        max: 5
-    }}]}}
-});
+$.ajax({
+    url: "/api/prosjekter/stemmer",
+    data: {},
+    success: visGraf
+})
+
+function visGraf(resultat) {
+    let prosjekt = [];
+
+    console.log("#reee");
 
 
+    var random1 = randomRGBA();
+    var random2 = randomRGBA();
+    var random3 = randomRGBA();
+//console.log(resultat[0])
 
-//brukt til inspirasjon
-//TODO fjern før prosjektet er ferdig
-new Chart(document.getElementById("chartjs-1"), {
-    type: 'bar',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 205, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(201, 203, 207, 0.2)'
-            ],
-            borderColor: [
-                'rgb(255, 99, 132)',
-                'rgb(255, 159, 64)',
-                'rgb(255, 205, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(54, 162, 235)',
-                'rgb(153, 102, 255)',
-                'rgb(201, 203, 207)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {scales: {yAxes: [{ticks: {beginAtZero: true}}]}}
-});
+    let label = resultat.map(a => a.prosjektnavn);
+    let data = resultat.map(a => a.gjennomsnittVerdi);
+    var myBar = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['prosjekter'],
+            datasets: [{
+                label: label,
+                data: data,
+                borderColor: [
+                    random1 + '1)',
+                    random2 + '1)',
+                    random3 + '1)'
+                ],
+                backgroundColor: [
+                    random1 + '0.1)',
+                    random2 + '0.1)',
+                    random3 + '0.1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        //max: 5
+                    }
+                }]
+            }
+        }
+    });
+}
 
 /*]]>*/
