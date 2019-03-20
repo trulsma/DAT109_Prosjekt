22adbe9c966835473dc7c4b22380fd125e051c75
S571736 <Sondre.Gjesdal@gmail.com>
2019-03-20 11:14:28 +0100

    forsøk på å få opp line chart med timestamps?

--- src/main/java/no/hvl/dat109/spring/controller/StemmeController.java
+++ src/main/java/no/hvl/dat109/spring/controller/StemmeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -46,6 +47,7 @@ public class StemmeController {
         List<ProsjektBean> prosjektListe = new ArrayList<>();
 
         prosjekter.forEach(prosjektListe::add);
+
 
         List<ProsjektMedStemmerBean> prosjekterMedStemmer = prosjektListe.stream().map(prosjekt ->
                 new ProsjektMedStemmerBean(prosjekt.getProsjektid(),
