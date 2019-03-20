8c14ff3cc112a046df8d8a93b4d2ca1e391cfab2
571530 <truls.martinsen13@gmail.com>
2019-03-20 12:17:33 +0100

    Legger til statistikk side for hver enkel side

--- src/main/java/no/hvl/dat109/spring/controller/StatistikkController.java
+++ src/main/java/no/hvl/dat109/spring/controller/StatistikkController.java
@@ -1,11 +1,15 @@
 package no.hvl.dat109.spring.controller;
 
+import no.hvl.dat109.spring.beans.ProsjektBean;
 import no.hvl.dat109.spring.service.Interfaces.IBedriftService;
 import no.hvl.dat109.spring.service.Interfaces.IProsjektService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import static no.hvl.dat109.prosjekt.Processing.getImagePath;
 
 @Controller
 public class StatistikkController {
@@ -22,4 +26,19 @@ public class StatistikkController {
     public String getStatistics() {
         return "adminpages/statistikk/statistikk.html";
     }
+
+
+    @GetMapping("/prosjekt/{id}/statistikk")
+    String getProsjektStatistikk(@PathVariable("id") int id, Model model) {
+        ProsjektBean prosjekt = prosjektService.getProsjektById(id);
+
+        if (prosjekt == null) {
+            return "error";
+        }
+
+        model.addAttribute("id", prosjekt.getProsjektid());
+
+        return "standpages/stand_statistikk";
+    }
+
 }
